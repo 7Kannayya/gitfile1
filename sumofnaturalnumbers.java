382d3d4d1d2abfe7a16850b27008317e1f09b6fe
7Kannayya <yalamanchiliravi69@gmail.com>
2024-04-03 16:52:18 +0530

    Natural numbers

---
+++ sumofnaturalnumbers.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Sum{
+	public static void main(String[]args){
+		int n=10,sum=0;
+		for(int i=0;i<=n;i++){
+			sum=sum+i;
+		}
+		System.out.println("Sum="+sum);
+	}
+}
